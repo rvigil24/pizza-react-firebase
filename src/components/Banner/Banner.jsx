879ad901dc56a836830fbba49f400dcb716cae5c
Rubén Vigil <ruben.vigil24@gmail.com>
2021-04-26 17:58:33 -0600

    Banner image added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,9 +4,10 @@ import styled from "styled-components";
 const BannerStyled = styled.div`
   height: 200px;
   border: 1px solid red;
-  background-image: url("https://res.cloudinary.com/dfrcck8j7/image/upload/v1619480606/pizza-react-firebase/pexels-polina-tankilevitch-4109128_cryctm.jpg");
+  background-image: url("https://res.cloudinary.com/dfrcck8j7/image/upload/v1619481227/pizza-react-firebase/pexels-polina-tankilevitch-4109084_ekcbcw.jpg");
   background-position: center;
   background-size: cover;
+  filter: contrast(105%);
 `;
 
 export const Banner = () => {
