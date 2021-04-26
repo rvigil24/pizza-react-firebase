612288904e3d542e2abc989ff71b1b941fa4114e
Rubén Vigil <ruben.vigil24@gmail.com>
2021-04-26 16:23:33 -0600

    google fonts added, install styled-components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import react from "react";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    body{
+        font-family: 'Open Sans', sans-serif;
+    }
+
+    h1, h2, h3{
+        font-family: 'Righteous', cursive;
+    }
+`;
 
 const App = () => {
     return (
+        <>
+        <GlobalStyle/>
         <div>app</div>
+        </>
     )
 }
 
