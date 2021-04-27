1149007c70ea3a3ae3e2db10947b6f89639be27d
Rubén Vigil <ruben.vigil24@gmail.com>
2021-04-26 21:11:35 -0600

    Dummy Food Data added to project

---
+++ src/FoodData.js
@@ -0,0 +1,37 @@
+export const FoodData = [
+    {
+        id: 1,
+        name: "Cheese Pizza",
+        img: "https://res.cloudinary.com/dfrcck8j7/image/upload/v1619487071/pizza-react-firebase/menu/pizza-cheese_lvjy82.jpg"
+    },
+    {
+        id: 2,
+        name: "Pepperoni Pizza",
+        img: "https://res.cloudinary.com/dfrcck8j7/image/upload/v1619486883/pizza-react-firebase/menu/pizza-pepperoni_nz9j6i.jpg"
+    },
+    {
+        id: 3,
+        name: "Margherita Pizza",
+        img: "https://res.cloudinary.com/dfrcck8j7/image/upload/v1619488567/pizza-react-firebase/menu/healthy-pizza_bvenhn_mi6jin.jpg"
+    },
+    {
+        id: 4,
+        name: "Burger",
+        img: "https://res.cloudinary.com/dfrcck8j7/image/upload/v1619487744/pizza-react-firebase/menu/burger_qrfwyq_m0two3.jpg"
+    },
+    {
+        id: 5,
+        name: "Fries",
+        img: "https://res.cloudinary.com/dfrcck8j7/image/upload/v1619488583/pizza-react-firebase/menu/french-fries_ogjwea_jxb8s8.jpg"
+    },
+    {
+        id: 6,
+        name: "Club Sandwich",
+        img: "https://res.cloudinary.com/dfrcck8j7/image/upload/v1619487772/pizza-react-firebase/menu/sandwich_lsm0cu_tnowct.jpg"
+    },
+    {
+        id: 7,
+        name: "Chicken Wings",
+        img: "https://res.cloudinary.com/dfrcck8j7/image/upload/v1619487866/pizza-react-firebase/menu/chicken-wings_p06aow_fg1uxo.jpg"
+    },
+]
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,8 +3,7 @@ import styled from "styled-components";
 
 const BannerStyled = styled.div`
   height: 200px;
-  border: 1px solid red;
-  background-image: url("https://res.cloudinary.com/dfrcck8j7/image/upload/v1619486582/pizza-react-firebase/background/pexels-ilya-alexeeff-5735845_jmvbvb.jpg");
+  background-image: url("https://res.cloudinary.com/dfrcck8j7/image/upload/v1619486735/pizza-react-firebase/background/pexels-ilya-alexeeff-5735845_kbbo1z.jpg");
   background-position: center;
   background-size: cover;
   filter: brightness(75%);
