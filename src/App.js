5f311c1f04430df63cf1f7df9724e41831fe17ea
Rubén Vigil <ruben.vigil24@gmail.com>
2021-04-26 21:52:05 -0600

    Global style created, clean up App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-import react from "react";
-import styled, { createGlobalStyle } from "styled-components";
+import { GlobalStyle } from "./styles/GlobalStyle";
 
 //pages
 import { Home } from "./pages/Home";
 
-const GlobalStyle = createGlobalStyle`
-*{
-    margin: 0;
-    padding: 0;
-}
-    body{
-        font-family: 'Open Sans', sans-serif;
-    }
 
-    h1, h2, h3{
-        font-family: 'Righteous', cursive;
-    }
-`;
 
 const App = () => {
     return (
---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+}
+    body{
+        font-family: 'Open Sans', sans-serif;
+    }
+
+    h1, h2, h3{
+        font-family: 'Righteous', cursive;
+    }
+`;
