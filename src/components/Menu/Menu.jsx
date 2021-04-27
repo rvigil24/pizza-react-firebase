6108493d73aa96694920579e13fa6ec8c4aa0f03
Rubén Vigil <ruben.vigil24@gmail.com>
2021-04-26 20:13:14 -0600

    Menu component added to home

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+
+import { FoodData } from "../../FoodData";
+
+import { FoodGrid, Food, FoodLabel } from "../FoodGrid/FoodGrid";
+
+const MenuStyled = styled.div`
+  margin: 0px 400px 50px 20px;
+  height: 1000px;
+`;
+
+export const Menu = () => {
+  return (
+    <MenuStyled>
+      <h1>Menu</h1>
+      <FoodGrid>
+        {FoodData.map((food) => {
+          return (
+            <Food key={food.id} img={food.img}>
+              <FoodLabel>{food.name}</FoodLabel>
+            </Food>
+          );
+        })}
+      </FoodGrid>
+    </MenuStyled>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 //components
 import { NavBar } from "../components/NavBar/NavBar";
 import { Banner } from "../components/Banner/Banner";
+import { Menu } from "../components/Menu/Menu";
 export const Home = () => {
   return (
     <>
       <NavBar />
       <Banner />
+      <Menu />
     </>
   );
 };
